f7c24679a0a121e50a8193257ba2133d8e4d2cfb
Ermy <erebong4193@gmail.com>
2025-01-07 22:23:50 +0800

    final commit 0.5

--- types/window.d.ts
+++ types/window.d.ts
@@ -2,8 +2,12 @@ import { Eip1193Provider } from "ethers";
 
 declare global {
   interface Window {
-    ethereum?: Eip1193Provider;
+    ethereum?: Eip1193Provider & {
+      request: (args: {method: string; params?: any[]}) => Promise<any>;
+      on: (event: string, callback: (...args: any[]) => void) => void;
+      removeListener: (event: string, callback: (...args: any[]) => void) => void;
+    };
   }
 }
 
-export {}; 
+export {};
